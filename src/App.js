e60542d1950d32ca1b619efca7d417dfbbdc31ad
Vishnu-IOT <93981819+Vishnu-IOT@users.noreply.github.com>
2025-11-27 11:22:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Duration from './components/duration';
+
+function App() {
+  return (
+    <div className="App">
+      <Duration />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/duration.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Duration = () => {
+    return (
+        <div>
+            <h1>Upload-Form</h1>
+            <select>
+                <option>Daily</option>
+                <option>Weekly</option>
+                <option>Monthly</option>
+                <option>Yearly</option>
+            </select>
+        </div>
+    )
+}
+
+export default Duration
